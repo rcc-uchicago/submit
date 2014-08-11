349080fdfdfd62ca629bb99e0a95558bb207a956
moniker001 <lim.tejong@gmail.com>
2014-08-11 12:23:20 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@
 var Hapi = require('hapi');
 var Good = require('good');
 var fs = require('fs');
+var multiparty = require('multiparty')
+  , http = require('http')
+  , util = require('util')
 
 var server = new Hapi.Server('localhost', 8000, { cors: true });
 
@@ -11,22 +14,12 @@ var handler = function(request, reply) {
 };
 
 var postHandler = function(request, reply) {
-    //console.log("rawPayload: " + request.rawPayload);
-    //console.log("Received POST from " + request.payload.name + "; id=" + (request.payload.id || 'anon'));
-/*
-    if (request.payload.uploadFile) {
-        var f = request.payload.uploadFile;
-        console.log("uploadFile " + f.originalFilename + " (" + f.size + " bytes) at " + f.path);
-        console.log("that you should persist to storage and remove from temp folder");
-        // Use fs for this one: http://nodejs.org/api/fs.html
-        Fs.unlink(f.path, function (err) {
-            if (err) throw err;
-                console.log('successfully deleted ' + f.path);
-        });
-    }
-*/
-    server.log("Hello "+request.payload.fname + " " + request.payload.lname);
-    //reply("Hello, "+request.payload.fname + " " + request.payload.lname + "!");
+  server.log("Hello " + request.payload.fname + " " + request.payload.lname);
+  //reply("Hello, " + request.payload.fname + " " + request.payload.lname + "!");
+  request.payload.file.pipe(fs.createWriteStream("test"));
+  if (request.payload.file) {
+    server.log("Received file: " + request.payload.file);
+  }
 }
 
 /*
@@ -47,7 +40,6 @@ var currentpositionApi = {
                 console.log(files)
 
             });
-
     }
 }
 */
@@ -60,20 +52,18 @@ server.route([
       reply('Hello ' + user + '!');
     }
   },
-/*
-  {
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-      reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-  },
-*/
 
   {
     method: 'POST',
     path: '/api/post',
-    handler: postHandler
+    config: {
+        payload:{
+              maxBytes: 209715200,
+              output:'stream',
+              parse: true
+        }, 
+        handler: postHandler
+    }
   },
 
   {
@@ -84,20 +74,7 @@ server.route([
     }
   }
 ]);
-/*
-server.route({
-    method: 'POST',
-    path: '/api/uploadfiles',
-    config: {
-          payload:{
-                maxBytes:209715200,
-                output:'stream',
-                parse: false
-          }, 
-          handler: currentposition.uploadFiles,
-    }
-});
-*/
+
 server.pack.register(Good, function (err) {
   if (err) {
       throw err; // something bad happened loading the plugin
@@ -110,3 +87,4 @@ server.pack.register(Good, function (err) {
   server.log(['error', 'database', 'read']);
 
 });
+
