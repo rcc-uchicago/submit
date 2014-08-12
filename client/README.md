7a70c2b9ff74ff411babafe3f2269be3d1b5625d
raguilera <raguilera@uchicago.edu>
2014-08-12 16:31:23 -0500

    removing diretory

--- client/README.md
+++ client/README.md
@@ -1,3 +1,10 @@
+<<<<<<< HEAD
 # Submit Client
 
 A simple front-end for submitting files and associated meta-data to a backend web server for validation and workflow tracking.
+=======
+# Submit
+
+A simple web service for submitting/validating file uploads and associated
+metadata.  We provide a sample web-client along with the web server.
+>>>>>>> 3ce89a214b94c67c6c82dda2ee160ac74a9aaf37
--- client/main.js
+++ client/main.js
@@ -35,7 +35,7 @@ function sendForm() {
   var uploadedFile = document.getElementById("chooser").files[0];
   formData.append("fname", fname);
   formData.append("lname", lname);
-  formData.append("file", uploadedFile);
+  formData.append("files", uploadedFile);
 
   var xhr = new XMLHttpRequest();
   xhr.open("POST", "http://localhost:8000/api/post", true);
@@ -62,6 +62,35 @@ function resetFunc() {
   document.getElementById("firstname").value = "";
   document.getElementById("lastname").value = "";
 };
+
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+var files = evt.dataTransfer.files; // FileList object.
+
+// files is a FileList of File objects. List some properties.
+var output = [];
+   for (var i = 0, f; f = files[i]; i++) {
+   output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+    f.size, ' bytes, last modified: ',
+    f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+  '</li>');
+   }
+document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+}
+
+function handleDragOver(evt) {
+  evt.stopPropagation();
+  evt.preventDefault();
+  evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+// Setup the dnd listeners.
+var dropZone = document.getElementById('drop_zone');
+  dropZone.addEventListener('dragover', handleDragOver, false);
+  dropZone.addEventListener('drop', handleFileSelect, false);
+
 
 loadfile();
 
