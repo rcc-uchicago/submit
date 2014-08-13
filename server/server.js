ef817edc12631f3523bcfc4dd8f89192f2acdf0e
moniker001 <lim.tejong@gmail.com>
2014-08-13 10:20:53 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,34 +15,19 @@ var handler = function(request, reply) {
 
 var postHandler = function(request, reply) {
   server.log("Hello " + request.payload.fname + " " + request.payload.lname);
-  //reply("Hello, " + request.payload.fname + " " + request.payload.lname + "!");
+  if (request.payload.aboutfile) {
+    server.log("About file: " + request.payload.aboutfile);
+  }
+
   request.payload.file.pipe(fs.createWriteStream("test"));
   if (request.payload.file) {
     server.log("Received file: " + request.payload.file);
   }
+
+  //reply("Hello, " + request.payload.fname + " " + request.payload.lname + "!");
 }
 
-/*
-var currentpositionApi = {
 
-    fs : require('fs'),
-    multiparty: require('multiparty'),
-    uploadFiles:function(req,reply){
-         var form = new currentpositionApi.multiparty.Form();
-            form.parse(req.payload, function(err, fields, files) {
-                currentpositionApi.fs.readFile(files.upload[0].path,function(err,data){
-                    var newpath = __dirname + "/"+files.upload[0].originalFilename;
-                    currentpositionApi.fs.writeFile(newpath,data,function(err){
-                        if(err) console.log(err);
-                        else console.log(files)
-                    })
-                })
-                console.log(files)
-
-            });
-    }
-}
-*/
 server.route([
   {
     method: 'GET',
