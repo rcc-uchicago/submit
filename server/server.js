f90a509aafa1e11f900302eee4ed0729edb2b4f5
moniker001 <lim.tejong@gmail.com>
2014-09-04 11:34:40 -0500

    updating server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ function getIPAddress() {
   var interfaces = os.networkInterfaces();
   for (var devName in interfaces) {
     var iface = interfaces[devName];
-
     for (var i = 0; i < iface.length; i++) {
       var alias = iface[i];
       if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
@@ -45,72 +44,56 @@ var handler = function(request, reply) {
 
 var postHandler = function(request, reply) {
   server.log("Connection made!")
+  server.log("Hello " + request.payload.fname + " " + request.payload.lname);
+	if (request.payload.upload0.hapi) {
+	  var fname0 = request.payload.upload0.hapi.filename;
+	  var save0 = fs.createWriteStream("./uploads/".concat(fname0));
+	  request.payload.upload0.pipe(save0);
+	  request.on('error', function(err) {
+	    server.log(err);
+	  });
+	  server.log("Received file: " + fname0);
+	}
 
-  //validation
-	var configSchema = Joi.object({
-		fname: Joi.string().regex(/^[a-zA-Z]+$/).required(),
-		lname: Joi.string().regex(/^[a-zA-Z]+$/).required()
-	});
-  var config = {
-		fname: request.payload.fname,
-		lname: request.payload.lname
-  }
-  var nameValid = Joi.validate(config, configSchema, {abortEarly: false});
-  if (nameValid.error) {
-    server.log("Name fields cannot contain numbers or symbols");
-  } else { //when validation tests pass
-    server.log("Hello " + request.payload.fname + " " + request.payload.lname);
+	if (request.payload.upload1.hapi) {
+	  var fname1 = request.payload.upload1.hapi.filename;
+	  var save1 = fs.createWriteStream("./uploads/".concat(fname1));
+	  request.payload.upload1.pipe(save1);
+	  request.on('error', function(err) {
+		  server.log(err);
+		});
+		server.log("Received file: " + fname1);
+	}
 
-		if (request.payload.upload0.hapi) {
-		  var fname0 = request.payload.upload0.hapi.filename;
-		  var save0 = fs.createWriteStream("./uploads/".concat(fname0));
-		  request.payload.upload0.pipe(save0);
-		  request.on('error', function(err) {
-		    server.log(err);
-		  });
-		  server.log("Received file: " + fname0);
-		}
+	if (request.payload.upload2.hapi) {
+	  var fname2 = request.payload.upload2.hapi.filename;
+	  var save2 = fs.createWriteStream("./uploads/".concat(fname2));
+	  request.payload.upload1.pipe(save2);
+	  request.on('error', function(err) {
+	    server.log(err);
+	  });
+	  server.log("Received file: " + fname2);
+	}
 
-		if (request.payload.upload1.hapi) {
-		  var fname1 = request.payload.upload1.hapi.filename;
-		  var save1 = fs.createWriteStream("./uploads/".concat(fname1));
-		  request.payload.upload1.pipe(save1);
-		  request.on('error', function(err) {
-		    server.log(err);
-		  });
-		  server.log("Received file: " + fname1);
-		}
+	if (request.payload.upload3.hapi) {
+	  var fname3 = request.payload.upload3.hapi.filename;
+	  var save3 = fs.createWriteStream("./uploads/".concat(fname3));
+	  request.payload.upload1.pipe(save3);
+	  request.on('error', function(err) {
+	    server.log(err);
+	  });
+	  server.log("Received file: " + fname3);
+	}
 
-		if (request.payload.upload2.hapi) {
-		  var fname2 = request.payload.upload2.hapi.filename;
-		  var save2 = fs.createWriteStream("./uploads/".concat(fname2));
-		  request.payload.upload1.pipe(save2);
-		  request.on('error', function(err) {
-		    server.log(err);
-		  });
-		  server.log("Received file: " + fname2);
-		}
-
-		if (request.payload.upload3.hapi) {
-		  var fname3 = request.payload.upload3.hapi.filename;
-		  var save3 = fs.createWriteStream("./uploads/".concat(fname3));
-		  request.payload.upload1.pipe(save3);
-		  request.on('error', function(err) {
-		    server.log(err);
-		  });
-		  server.log("Received file: " + fname3);
-		}
-
-		if (request.payload.upload4.hapi) {
-		  var fname4 = request.payload.upload4.hapi.filename;
-		  var save4 = fs.createWriteStream("./uploads/".concat(fname4));
-		  request.payload.upload1.pipe(save4);
-		  request.on('error', function(err) {
-		    server.log(err);
-		  });
-		  server.log("Received file: " + fname4);
-		}
-  }
+	if (request.payload.upload4.hapi) {
+	  var fname4 = request.payload.upload4.hapi.filename;
+	  var save4 = fs.createWriteStream("./uploads/".concat(fname4));
+	  request.payload.upload1.pipe(save4);
+	  request.on('error', function(err) {
+	    server.log(err);
+	  });
+	  server.log("Received file: " + fname4);
+	}
 }
 
 server.route([
