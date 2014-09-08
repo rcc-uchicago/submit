96289132b4f77946eee432ef52e7cf08a918cc22
Jason Voigt <jvoigt@uchicago.edu>
2014-09-08 11:50:13 -0500

    revise route

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -1,54 +1,14 @@
 var postHandler = function(request, reply) {
-  console.log("Connection made!")
-  console.log("Hello " + request.payload.fname + " " + request.payload.lname);
-	if (request.payload.upload0.hapi) {
-	  var fname0 = request.payload.upload0.hapi.filename;
-	  var save0 = fs.createWriteStream("./uploads/".concat(fname0));
-	  request.payload.upload0.pipe(save0);
+  console.log(request.payload.form);
+	if (request.payload.upload.hapi) {
+	  var name = request.payload.upload.hapi.filename;
+    var path = __dirname + "/uploads/" + name
+	  var file = fs.createWriteStream(path);
+	  request.payload.upload.pipe(file);
 	  request.on('error', function(err) {
 	    console.log(err);
 	  });
-	  console.log("Received file: " + fname0);
-	}
-
-	if (request.payload.upload1.hapi) {
-	  var fname1 = request.payload.upload1.hapi.filename;
-	  var save1 = fs.createWriteStream("./uploads/".concat(fname1));
-	  request.payload.upload1.pipe(save1);
-	  request.on('error', function(err) {
-		  console.log(err);
-		});
-		console.log("Received file: " + fname1);
-	}
-
-	if (request.payload.upload2.hapi) {
-	  var fname2 = request.payload.upload2.hapi.filename;
-	  var save2 = fs.createWriteStream("./uploads/".concat(fname2));
-	  request.payload.upload1.pipe(save2);
-	  request.on('error', function(err) {
-	    console.log(err);
-	  });
-	  console.log("Received file: " + fname2);
-	}
-
-	if (request.payload.upload3.hapi) {
-	  var fname3 = request.payload.upload3.hapi.filename;
-	  var save3 = fs.createWriteStream("./uploads/".concat(fname3));
-	  request.payload.upload1.pipe(save3);
-	  request.on('error', function(err) {
-	    console.log(err);
-	  });
-	  console.log("Received file: " + fname3);
-	}
-
-	if (request.payload.upload4.hapi) {
-	  var fname4 = request.payload.upload4.hapi.filename;
-	  var save4 = fs.createWriteStream("./uploads/".concat(fname4));
-	  request.payload.upload1.pipe(save4);
-	  request.on('error', function(err) {
-	    console.log(err);
-	  });
-	  console.log("Received file: " + fname4);
+	  console.log("Received file: " + name);
 	}
 }
 
