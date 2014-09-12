8e3b246b926a71f7f73b748178aff26b132c0b84
Jason Voigt <jvoigt@uchicago.edu>
2014-09-12 15:58:28 -0500

    add drag-n-drop to client

--- server/public/client/load.js
+++ server/public/client/load.js
@@ -1,18 +1,39 @@
 var $ = function (id) { return document.getElementById(id) };
 
-function load() {
-    var File = this.files[0];
-    /*
+function load(files) {
+    var File = files[0];
+    $('inputFile').textContent = File.name;
+
+    /* OPTIONALLY READ AND VALIDATE FILE CONTENT
     var reader = new FileReader();
     reader.onload = function(file) {
-        reader.readAsText(File);
+        text = this.result     // FILE CONTENT (UTF-8 STRING)
+        validate(text)
     };
+    reader.readAsText(File);
     */
-    $('inputFile').textContent = File.name;
 };
 
-$('chooser').addEventListener('change', load);
+
+// Set `inputFile` as button triggering file chooser
 $('inputFile').addEventListener('click', function (e) {
-  chooser.click();
-  e.preventDefault(); // prevent navigation to "#"
+    chooser.click();
+    e.preventDefault(); // prevent navigation to "#"
 }, false);
+
+
+// Set `window` as drag-n-drop zone
+window.addEventListener('dragover', dragover, false);
+window.addEventListener('drop', drop, false);
+
+function dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
+}
+
+function drop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    load(e.dataTransfer.files);
+}
--- server/server.js
+++ server/server.js
@@ -1,29 +1,31 @@
 var Hapi = require('hapi');
 var Path = require('path');
 var parse = require('minimist');
-var getIP = require('lib/ip');
 var routes = require('lib/routes');
+var ip = require('lib/ip');
 
-var argOptions = {
-    alias: {
-        host: 'h',
-        port: 'p'
+
+var config = {
+    argv: {
+        alias: {
+            host: 'h',
+            port: 'p'
+        },
+        "default": {
+            host: ip(),     // detect an ip address
+            port: 8001
+        }
     },
-    "default": {
-        host: getIP(),
-        port: 8001
+    server: {
+        // cors: true,
+        // tls: require('lib/tls-config.js'),
+        files: { relativeTo: Path.join(__dirname, 'public') }
     }
 };
 
-var argv = parse(process.argv.slice(2), argOptions);
+var argv = parse(process.argv.slice(2), config.argv);
 
-var options = {
-    // cors: true,
-    // tls: require('lib/tls-config.js'),
-    files: { relativeTo: Path.join(__dirname, 'public') }
-};
-
-var server = new Hapi.Server(argv.host, argv.port, options);
+var server = new Hapi.Server(argv.host, argv.port, config.server);
 
 server.route(routes);
 
