ee3bad076e06db23fa18cf0950032342b4ef230f
moniker001 <lim.tejong@gmail.com>
2014-09-18 10:31:52 -0500

    Update labtest.js

--- server/test/labtest.js
+++ server/test/labtest.js
@@ -1,27 +1,60 @@
+//Testing hapi server with lab
+var Hapi = require('hapi');
+var fs = require('fs');
+var FormData = require('form-data');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
 var server = require(__dirname + '/../');
+var describe = lab.describe;
+var it = lab.it;
+var expect = Lab.expect;
 //-----------------------------------------------------------------------------
 // credentials
-var username = 'Bob';
-var password = 'password';
+var username = ''; //do not commit with actual username
+var password = ''; // do not commit with actual password
+var firstname = 'Bob';
+var lastname = 'Jones';
+var filename = 'test2.csv';
 //-----------------------------------------------------------------------------
 
+var postHandler = function(request, reply) {
+    console.log('Connection made!')
+    var payload = request.payload;
+    //console.log(payload);
+    console.log('Hello ' + payload.fname + ' ' + payload.lname);
+    var data = payload.upload;
+    
+    if (data) {
+        if (data.hapi) {
+            var filename = data.hapi.filename;
+            var path = __dirname + '/../uploads/' + filename;
+            var save = fs.createWriteStream(path);
+            save.on('error', function(err) { console.log(err) });
+            //data.pipe(save);
+            console.log('Received file: ' + filename);
+            request.on('error', function(err) {
+                console.log(err);
+            });
+        }
+    }
+    
+    reply(payload);
+};
 
-lab.experiment('fileupload', function() {
+describe('login', function() {
 
-    lab.test('require authentication', function(done) {
+    it('login unsuccessful', function(done) {
         var options = {
             method: 'GET',
             url: '/submit',
         }
         server.inject(options, function(response) {
-            Lab.expect(response.statusCode).to.equal(401); // 401 not authorized
+            expect(response.statusCode).to.equal(401);
             done();
         })
     });
-	
-    lab.test('authentication successful', function(done) {
+
+    it('login successful', function (done) {
         var options = {
             method: 'GET',
             url: '/submit',
@@ -30,14 +63,30 @@ lab.experiment('fileupload', function() {
                 password: password
             }
         }
-        server.inject(options, function(response) {
-            Lab.expect(response.statusCode).to.equal(200);
+        server.inject(options, function (res) {
+            expect(res.statusCode).to.equal(200);
             done();
         })
     });
 
-    /*
-    lab.test('name field submission', function(done) {
+    it('payload test', function (done) {
+        var server = new Hapi.Server();
+        server.route({ 
+            method: 'POST', 
+            path: '/submit', 
+            config: { handler: postHandler }
+        });
+
+        var payload = {
+            fname: 'Bob',
+            lname: 'Jones',
+            upload: {
+                hapi: {
+                    filename: filename
+                }
+            }
+        };
+
         var options = {
             method: 'POST',
             url: '/submit',
@@ -45,29 +94,24 @@ lab.experiment('fileupload', function() {
                 username: username,
                 password: password
             },
-            payload: {  // PROBLEM: THIS IS NOT A PROPER PAYLOAD
-                fname: 'Bob',
-                lname: 'Jones',
-                uploads: {
-                    hapi: { filename: 'test2.csv',
-                        headers: { 
-                            'content-disposition': 'form-data; name="upload"; filename="test2.csv"',
-                            'content-type': 'text/csv'
-                        }
-                    }
-                }
-            }
-        }
-        server.inject(options, function(response) {
-            var result = response.result,
-            payload = options.payload;
-            Lab.expect(response.statusCode).to.equal(200);
-            Lab.expect(result.fname).to.equal(payload.fname);
-            Lab.expect(result.lname).to.equal(payload.lname);
-            Lab.expect(result.upload).to.equal(payload.upload);
+            headers: headers,
+            payload: payload
+        };
+        var headers = {
+            'content-Type': 'multipart/form-data; boundary=----------------------------24ec3aaa82b3'
+        };
+        server.inject(options, function (res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.result.fname).to.equal(payload.fname);
+            expect(res.result.lname).to.equal(payload.lname);
+            expect(res.result.upload.hapi.filename).to.equal(payload.upload.hapi.filename);
+
             done();
-        })
+        });
     });
-    */
+    
+    
 });
+
+
 
